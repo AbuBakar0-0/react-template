ac2c8d6d03db308bd85c5b564120075d850195d5
Abu Bakar <abubakar.80187@gmail.com>
2025-04-08 17:48:55 +0500

    update

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center bg-gray-400'>
+        <span className='text-9xl font-bold text-white'>Home</span>
+    </div>
+  )
+}
+
+export default Home
